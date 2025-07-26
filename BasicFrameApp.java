27cc4374a6094c1da4f59daf3b6072e3503dc6fe
Vutomiey <vutomimabedlhe15@gmail.com>
2025-07-26 22:14:59 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author vutom
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //declare vaariables
+        MyFirstFrame myFrame;
+        
+        //instatiate the frame
+        myFrame = new MyFirstFrame();
+        
+    }
+    
+}
